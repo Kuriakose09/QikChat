d07080994747db28f0e921708fdc25216911e42c
Kuriakose Antony <kuriakoseantony09@gmail.com>
2025-10-15 23:36:18 +0530

    project on progress(create models)

--- Qikchat_project/accounts/admin.py
+++ Qikchat_project/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Userprofile
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'online_status', 'created_at')
+# , UserProfileAdmin
+admin.site.register(Userprofile, UserProfileAdmin)
--- Qikchat_project/accounts/models.py
+++ Qikchat_project/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Userprofile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    profile_picture = models.ImageField(upload_to='profile_pics/')
+    online_status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.full_name
+
+# class User_Profile(AbstractUser):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     Full_Name = models.CharField(max_length=100)
+#     Profile_picture = models.ImageField()
+#     Online_status = models.BooleanField()
+#     Created_date = models.DateField()
